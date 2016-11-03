e5c4e94e0ec61b0631a6d742fe64144c2594b460
Rob Nichols <robnichols@warwickshire.gov.uk>
2016-11-03 15:49:06 +0000

    Add UkAddressParser.parse method

--- lib/uk_address_parser.rb
+++ lib/uk_address_parser.rb
@@ -2,5 +2,7 @@ require "uk_address_parser/version"
 require "uk_address_parser/address_parser"
 
 module UkAddressParser
-  # Your code goes here...
+  def self.parse(address)
+    AddressParser.new(address).details
+  end
 end
--- test/uk_address_parser_test.rb
+++ test/uk_address_parser_test.rb
@@ -4,4 +4,20 @@ class UkAddressParserTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::UkAddressParser::VERSION
   end
+
+  def test_parse
+    address = 'Flat 1, Bubble House, 12 Long Road, Someton, Worcestershire, WR1 1XX'
+    expected = {
+      flat: "Flat 1",
+      house_number: "12",
+      house_name: "Bubble House",
+      street: "Long Road",
+      street2: nil,
+      street3: nil,
+      town: "Someton",
+      county: "Worcestershire",
+      postcode:"WR1 1XX"
+    }
+    assert_equal expected, UkAddressParser.parse(address)
+  end
 end
